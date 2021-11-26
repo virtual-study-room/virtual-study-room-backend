f3c2e1607f6d6c1c206e14380884597efb3ecc73
Matthew Nieva <matthewcn56@gmail.com>
2021-11-25 17:13:15 -0800

    uses bcrypt to hash password, and adds user authentication

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 import { model, Schema, Model, Document } from "mongoose";
+import * as bcrypt from "bcrypt";
 
 //definition of what info a user should contain
 interface UserType extends Document {
@@ -11,6 +12,31 @@ interface UserType extends Document {
 const UserSchema: Schema = new Schema({
   username: { type: String, required: true, unique: true },
   bio: { type: String, required: false },
+  password: { type: String, required: true },
+});
+
+//mongoose hook to encrypt password before saving
+UserSchema.pre("save", function (next) {
+  //catching process not there
+  if (!process.env.SALTING_ROUNDS) return;
+  const user = this;
+  console.log(user);
+  if (!user.isModified || !user.isNew) {
+    //don't rehash if old user
+    next();
+  } else {
+    const saltingRounds = Number(process.env.SALTING_ROUNDS);
+    bcrypt.hash(user.password, saltingRounds, (err, hash) => {
+      if (err) {
+        console.log("Error hashing password for user", user.username);
+        console.log(err);
+        next(err);
+      } else {
+        user.password = hash;
+        next();
+      }
+    });
+  }
 });
 
 //let our database add new users by exporting our User model
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,36 +1,76 @@
 import { Router, Request, Response } from "express";
 const express = require("express");
 const router: Router = express.Router();
+import * as bcrypt from "bcrypt";
 import { User } from "../models/User";
+
 interface UserInfoType {
   username: string;
-  bio: string;
+  bio?: string;
+}
+
+interface AuthRequestType {
+  username: string;
+  password: string;
 }
 
 //route that tries to add a user's profile to the database if it doesn't exist yet
-router.post("/addInfo", async (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   //handle db not being initialized yet
-  const userInfo: UserInfoType = req.body;
-  const newUserProfile: UserInfoType = {
+  const userInfo: AuthRequestType = req.body;
+  const newUserProfile: AuthRequestType = {
     username: userInfo.username,
-    bio: userInfo.bio,
+    password: userInfo.password,
   };
   //attempt to add user info of a new user:
+  //attempt to create new user in model if doesn't exist, throw error if user is already there.
+  const newUser = new User({
+    username: newUserProfile.username,
+    password: userInfo.password,
+  });
+
+  await newUser.save((err, user) => {
+    if (!err) {
+      res
+        .status(200)
+        .send("Successfully registered: " + newUserProfile.username);
+    } else {
+      res.status(500).send(err.message);
+    }
+  });
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const requestInfo: AuthRequestType = req.body;
   try {
-    //attempt to create new user in model if doesn't exist, throw error if user is already there.
-    const newUser = new User({
-      username: newUserProfile.username,
-      bio: newUserProfile.bio,
+    const userInfo = await User.findOne({
+      username: requestInfo.username,
+      //TODO: Fix any
     });
 
-    await newUser.save();
+    if (!userInfo) {
+      res.status(401).send("Invalid username");
+      return;
+    }
 
-    res
-      .status(200)
-      .send("Successfully added user info of: " + newUserProfile.username);
-  } catch (error: any) {
-    console.error(error);
-    res.status(400).send(error.message);
+    //check to see if password matches
+    const correctPassword = await bcrypt.compare(
+      requestInfo.password,
+      userInfo.get("password")
+    );
+
+    if (!correctPassword) {
+      res.status(401).send("Invalid Password");
+      return;
+    } else {
+      res.status(200).send("User signed in.");
+      return;
+    }
+
+    //
+  } catch (err: any) {
+    console.error(err);
+    res.status(400).send(err.message);
   }
 });
 
@@ -47,11 +87,11 @@ router.get("/getInfo", async (req: Request, res: Response) => {
     //if the user exists, send its info back in response. If not, throw error saying user could not be found.
     userInfo
       ? res.status(200).send({
-        user: userInfo,
-      })
+          user: userInfo,
+        })
       : res
-        .status(404)
-        .send("Error: Could not find requested user: " + requestedUser);
+          .status(404)
+          .send("Error: Could not find requested user: " + requestedUser);
   } catch (error: any) {
     console.error(error);
     res.status(400).send(error.message);
@@ -76,19 +116,20 @@ router.post("/updateInfo", async (req: Request, res: Response) => {
       await oldUser.save();
       res
         .status(200)
-        .send("Successfully updated user info of: " + updatedUserProfile.username);
-    }
-    else {
+        .send(
+          "Successfully updated user info of: " + updatedUserProfile.username
+        );
+    } else {
       res
         .status(404)
-        .send("Error: Could not find requested user: " + updatedUserProfile.username);
+        .send(
+          "Error: Could not find requested user: " + updatedUserProfile.username
+        );
     }
-
   } catch (error: any) {
     console.error(error);
     res.status(400).send(error.message);
   }
-
 });
 
 //route that deletes user data
@@ -105,8 +146,7 @@ router.delete("/deleteInfo", async (req: Request, res: Response) => {
       res
         .status(200)
         .send("Successfully deleted user info of: " + userQuery.username);
-    }
-    else {
+    } else {
       res
         .status(404)
         .send("Error: Could not find requested user: " + userQuery.username);
@@ -115,14 +155,8 @@ router.delete("/deleteInfo", async (req: Request, res: Response) => {
     console.error(error);
     res.status(400).send(error.message);
   }
-
 });
 
-
 // // add modify and add delete, plus structure
-
-
-
-
 
 module.exports = router;
