4b1c9511d102e0723bc602ab762eaea30db3ce45
Matthew Wang <matthewjwang5@g.ucla.edu>
2021-11-29 20:33:54 -0800

    Added tuple feature for the items array in a todo list
    This is done in order to allow for the implementation and saving of the bool, whether or not a job is finished.

--- src/models/toDo.ts
+++ src/models/toDo.ts
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 import { ObjectID } from "bson";
 import { model, Schema, Model, Document } from "mongoose";
 
-//definition of what info a user should contain
+//definition of what info a to do list should contain
 export interface ToDoListBody extends Document {
     userID: string;
     title: string;
     date: Date;
     trashed: boolean;
-    items?: string[];
+    items?: [string, boolean][];
 }
 
 export interface ToDoList {
@@ -16,10 +16,10 @@ export interface ToDoList {
     title: string;
     date?: Date;
     trashed?: boolean;
-    items?: string[];
+    items?: [string,boolean][];
 }
 
-//mongoose's definition of our user with the same shape as our interface
+//mongoose's definition of our to list with the same shape as our interface
 const ToDoSchema: Schema = new Schema({
     userID: { type: String, required: true },
     title: { type: String, required: true },
@@ -28,6 +28,5 @@ const ToDoSchema: Schema = new Schema({
     items: { type: Array, required: false }
 });
 
-
-//let our database add new ToDoLists by exporting our User model
+//let our database add new ToDoLists by exporting our ToDo model
 export const ToDo: Model<ToDoListBody> = model("ToDo", ToDoSchema);
